e49c73c26841ec2ecff6e2a841950a833c07c24b
Ahmatjan <Hyper-Hack@outlook.com>
2023-03-24 16:35:07 +0800

    Complete the plugin

--- main.ts
+++ main.ts
@@ -1,85 +1,80 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, getIcon, Setting, Command, ToggleComponent, MenuItem } from 'obsidian';
+interface InsertRTLSettings {
 	mySetting: string;
+	showStatusBar: boolean;
+	showRightClick: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: InsertRTLSettings = {
+	mySetting: 'default',
+	showStatusBar: true,
+	showRightClick: true,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class InsertInvisibleRTL extends Plugin {
+	settings: InsertRTLSettings;
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		const InsertInvisibleCharCommand: Command = {
+			id: 'invisibleCharacter',
+			name: 'Insert invisible Character',
+			editorCallback:(editor: Editor, view: MarkdownView) => {
+				let noticeContent = document.createDocumentFragment();
+				noticeContent.createEl('div', {text: '🔔 Character has been inserted',
+				attr: {style: 'font-size: 1.2em;'}});
+				// @ts-ignore
+				if (editor.replaceSelection('؜')) {
+					new Notice(noticeContent, 2000);
 				}
 			}
-		});
+		};
+	
+		const onClickStatusBarItem = (evt: MouseEvent) => {
+			if (2 === evt.button) {
+				new MoreInfoModal(this.app).open();
+			} else {
+				//@ts-ignore
+				this.app.commands.executeCommandById(InsertInvisibleCharCommand.id);
+			}
+		}
+		
+		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		var statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setAttr("id", "statusButton");
+		statusBarItemEl.setAttr("aria-label", "Insert a Invisible RTL Character (click right for more info)");
+		statusBarItemEl.setAttr("aria-label-position", "top");
+		statusBarItemEl.addClass("mod-clickable");
+		statusBarItemEl.onClickEvent((this, onClickStatusBarItem));
+		statusBarItemEl.style.display = "block";
+		// @ts-ignore
+		const languageIcon1: any = (0, getIcon('pilcrow'));
+		languageIcon1.style.transform = "rotateY(180deg)";
+		statusBarItemEl.appendChild(languageIcon1);
+		statusBarItemEl =  statusBarItemEl.createEl("span");
+		statusBarItemEl.setText('InsertRTL');
+		statusBarItemEl.style.marginLeft = "5px";
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new TabsForSettings(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addCommand(InsertInvisibleCharCommand);
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu) => {
+				if (this.settings.showRightClick) {
+					menu.addItem((item) => {
+						item.setTitle("InsertRTL")
+						.setIcon("pilcrow")
+						.onClick((evt: MouseEvent) => {
+							onClickStatusBarItem(evt);
+						});
+					});
+				}
+				
+			})
+		);
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -89,16 +84,29 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async changeStatusBar() {
+		// get element by id
+		var statusBarItemEl = document.getElementById("statusButton");
+		// don't forget to check for null
+		if (statusBarItemEl === null) {
+			return;
+		}
+		statusBarItemEl.style.display = this.settings.showStatusBar ? "block" : "none";
+	}
 }
 
-class SampleModal extends Modal {
+class MoreInfoModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		// make the word AFTER bold and red
+		contentEl.innerHTML = 'This Plugin Insert a invisible RTL Character (U+061C) in Unicode' +
+		 '<h1 style="color:#ff0000; text-align: center;">AFTER</h1>' + 
+		 'the cursor, so that other Plugins or programs can correctly display RTL text even if the First Character is in LTR languages.';
 	}
 
 	onClose() {
@@ -107,10 +115,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class TabsForSettings extends PluginSettingTab {
+	plugin: InsertInvisibleRTL;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: InsertInvisibleRTL) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +128,28 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Plugin Button Display.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		.setName('Show/Hilde from Status Bar')
+		.setDesc('turn on to show insert button, or turn off to hide.')
+		.addToggle((toggle) => toggle.setValue(this.plugin.settings.showStatusBar)
+			.onChange((value) => {
+				this.plugin.settings.showStatusBar = !this.plugin.settings.showStatusBar;
+				this.plugin.saveSettings();
+				this.plugin.changeStatusBar();
+			})
+		);
+
+		// add new right click menu
+		new Setting(containerEl)
+		.setName('Show/Hilde from Right Click Menu')
+		.setDesc('turn on to show insert button, or turn off to hide.')
+		.addToggle((toggle) => toggle.setValue(this.plugin.settings.showRightClick)
+			.onChange((value) => {
+			this.plugin.settings.showRightClick = !this.plugin.settings.showRightClick;
+			this.plugin.saveSettings();
+		}));
+		
 	}
 }
